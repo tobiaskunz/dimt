f02f39b0aedd6a5bdda1ded98091986f29d1e476
Tobias Kunz <tobias@gatech.edu>
2015-05-24 14:37:19 -0700

    Calculate minimum time without solving quadratic equations.
    This improves efficiency and reduces code length.

--- DoubleIntegrator.h
+++ DoubleIntegrator.h
@@ -159,75 +159,6 @@ private:
 		return d * d;
 	}
 
-	static void getPPTime(double startVelocity, double goalVelocity, double distance, double acceleration1,
-	                      double* tMin1, double* tMin2, double* tMax1, double* tMax2)
-	{
-		if(startVelocity == goalVelocity && distance == 0.0) {
-			if(tMin1) {
-				*tMin1 = 0.0;
-				*tMin2 = 0.0;
-			}
-			if(tMax1) {
-				*tMax1 = 0.0;
-				*tMax2 = 0.0;
-			}
-			return;
-		}
-
-		const double a = acceleration1;
-		const double b = 2.0 * startVelocity;
-		//const double c = (squared(goalVelocity) - squared(startVelocity)) / 2.0 / acceleration2 - distance;
-		const double c = 0.5 * (startVelocity + goalVelocity) * ((goalVelocity - startVelocity) / -acceleration1) - distance;
-
-		const double radicand = b*b - 4.0*a*c;
-		assert(radicand >= 0.0);
-		
-		// numerically stable solution to the quadratic equation
-		const double q = -0.5 * (b + sign(b) * sqrt(radicand));
-
-		if(a * sign(b) >= 0.0) {
-			if(tMin1) *tMin1 = q / a;
-			if(tMax1) *tMax1 = c / q;
-		}
-		else {
-			if(tMin1) *tMin1 = c / q;
-			if(tMax1) *tMax1 = q / a;
-		}
-
-		if(tMin1) {
-			*tMin2 = *tMin1 - (goalVelocity - startVelocity) / acceleration1;
-			if(*tMin1 < 0.0 || *tMin2 < 0.0) {
-				*tMin1 = std::numeric_limits<double>::infinity();
-				*tMin2 = std::numeric_limits<double>::infinity();
-			}
-		}
-
-		if(tMax1) {
-			*tMax2 = *tMax1 - (goalVelocity - startVelocity) / acceleration1;
-			if(*tMax1 < 0.0 || *tMax2 < 0.0) {
-				*tMax1 = std::numeric_limits<double>::infinity();
-				*tMax2 = std::numeric_limits<double>::infinity();
-			}
-		}
-	}
-
-	static double getPLPTime(double startVelocity, double goalVelocity, double distance, double maxVelocity, double acceleration1)
-	{
-		assert(std::abs(startVelocity) <= maxVelocity && std::abs(goalVelocity) <= maxVelocity);
-
-		const double boundaryVelocity = sign(acceleration1) * maxVelocity;
-		const double timeToBoundary1 = (boundaryVelocity - startVelocity) / acceleration1;
-		const double timeToBoundary2 = (goalVelocity - boundaryVelocity) / -acceleration1;
-		const double distanceToBoundary = 0.5 * (2.0 * squared(boundaryVelocity) - squared(startVelocity) - squared(goalVelocity)) / acceleration1;
-		const double boundaryTime = (distance - distanceToBoundary) / boundaryVelocity;
-
-		if(boundaryTime < 0.0)
-			return std::numeric_limits<double>::infinity();
-		else {
-			return timeToBoundary1 + boundaryTime + timeToBoundary2;
-		}
-	}
-
 	static Trajectory1D getMinAcceleration(double startPosition, double startVelocity, double goalPosition, double goalVelocity, double time, double maxVelocity)
 	{
 		Trajectory1D trajectory;
@@ -268,120 +199,46 @@ private:
 		return trajectory;
 	}
 
-	static void adjustForInfeasibleIntervals(int i, double time, double maxTime,
-	                                         const Eigen::Ref<const Vector>& startVelocities, const Eigen::Ref<const Vector>& goalVelocities, const Eigen::Ref<const Vector>& distances,
-	                                         const Eigen::Ref<const Vector>& firstAccelerations, const Eigen::Ref<const Vector>& maxAccelerations, const Eigen::Ref<const Vector>& maxVelocities,
-	                                         double& minTime, std::pair<double, double>* infeasibleIntervals, int& limitDof)
-	{
-		if(time >= maxTime) {
-			minTime = time;
-			return;
-		}
-
-		if(time < minTime) {
-			infeasibleIntervals[i] = getInfeasibleInterval(startVelocities[i], goalVelocities[i], distances[i],
-			                                               firstAccelerations[i], maxAccelerations[i], maxVelocities[i]);
-		}
-		else {
-			minTime = time;
-			if(limitDof != -1) {
-				infeasibleIntervals[limitDof] = getInfeasibleInterval(startVelocities[limitDof], goalVelocities[limitDof], distances[limitDof],
-				                                                      firstAccelerations[limitDof], maxAccelerations[limitDof], maxVelocities[limitDof]);
-			}
-			limitDof = i;
-		}
-
-		int j = 0;
-		while(j <= i && minTime < maxTime) {
-			if(j != limitDof) {
-				if(infeasibleIntervals[j].first < minTime && minTime < infeasibleIntervals[j].second) {
-					minTime = infeasibleIntervals[j].second;
-					j = 0;
-					if(limitDof != -1) {
-						infeasibleIntervals[limitDof] = getInfeasibleInterval(startVelocities[limitDof], goalVelocities[limitDof], distances[limitDof],
-						                                                      firstAccelerations[limitDof], maxAccelerations[limitDof], maxVelocities[limitDof]);
-						limitDof = -1;
-					}
-				}
-			}
-			j++;
-		}
-	}
-
 public:
-
-	static double getMinTime1D(double startVelocity, double goalVelocity, double distance,
+	static double getMinTime1D(double velocity1, double velocity2, double distance,
 	                           double maxAcceleration, double maxVelocity, double maxTime,
-	                           double& acceleration1)
+	                           std::pair<double, double>& infeasibleInterval)
 	{
-		// determine distance travelled while accelerating from start velocity to goal velocity
-		const double accelerationTime = std::abs(goalVelocity - startVelocity) / maxAcceleration;
-		if(accelerationTime >= maxTime)
-			return accelerationTime;
-		const double accelerationDistance = 0.5 * (startVelocity + goalVelocity) * accelerationTime;
-		
-		// determine whether to accelerate at the upper or lower limit first
+		if(velocity1 > velocity2)
+			std::swap(velocity1, velocity2);
+		double velocity1Squared = velocity1 * velocity1;
+		double velocity2Squared = velocity2 * velocity2;
+
+		const double accelerationDistance = 0.5 * (velocity2Squared - velocity1Squared) / maxAcceleration;
 		const double additionalDistance = distance - accelerationDistance;
+		
+		if(additionalDistance < 0.0)
+			std::swap(velocity1Squared, velocity2Squared);
 
-		acceleration1 = sign(additionalDistance) * maxAcceleration;
+		const double absVelocity = sqrt(maxAcceleration * std::abs(additionalDistance) + velocity2Squared);
+		const double velocity = sign(additionalDistance) * absVelocity;
+		
+		double time = std::abs(velocity1 + velocity2 - 2.0 * velocity) / maxAcceleration;
 
-		double time1, time2;
-		getPPTime(startVelocity, goalVelocity, distance, acceleration1, NULL, NULL, &time1, &time2);
-		double time = time1 + time2;
+		if(absVelocity > maxVelocity)
+			time += squared(absVelocity - maxVelocity) / (maxVelocity * maxAcceleration);
 
-		assert(time >= 0.0);
-		assert(time != std::numeric_limits<double>::infinity());
+		if(time >= maxTime)
+			return time;
 
-		if(time < maxTime && std::abs(startVelocity + acceleration1 * time1) >= maxVelocity) {
-			time = getPLPTime(startVelocity, goalVelocity, distance, maxVelocity, acceleration1);
-		}
-
-		return time;
-	}
-
-	static std::pair<double, double> getInfeasibleInterval(double startVelocity, double goalVelocity, double distance,
-		double acceleration1, double maxAcceleration, double maxVelocity)
-	{
-		std::pair<double, double> infeasibleInterval;
-
-		if(startVelocity * goalVelocity <= 0.0 || acceleration1 * startVelocity < 0.0) {
-			// If minimum-time solution goes through zero-velocity, there is no infeasible time interval, because we can stop and wait at zero-velocity
+		const double zeroDistance = 0.5 * (velocity1Squared + velocity2Squared) / maxAcceleration;
+		if(velocity1 * velocity2 <= 0.0 || additionalDistance * velocity1 < 0.0 || zeroDistance < std::abs(distance)) {
 			infeasibleInterval.first = std::numeric_limits<double>::infinity();
 			infeasibleInterval.second = std::numeric_limits<double>::infinity();
 		}
-		// for all cases below: sign(startVelocity) == sign(goalVelocity) == sign(distance) == sign(additionalDistance) == sign(acceleration1) == -sign(acceleration2)
 		else {
-			double zeroTime1 = std::abs(startVelocity) / maxAcceleration;
-			double zeroTime2 = std::abs(goalVelocity) / maxAcceleration;
-			double zeroDistance = zeroTime1 * startVelocity / 2.0 + zeroTime2 * goalVelocity / 2.0;
-			if(std::abs(zeroDistance) < std::abs(distance)) {
-				infeasibleInterval.first = std::numeric_limits<double>::infinity();
-				infeasibleInterval.second = std::numeric_limits<double>::infinity();
-			}
-			else {
-				double timeLow1, timeLow2, timeHigh1, timeHigh2;
-				getPPTime(startVelocity, goalVelocity, distance, -acceleration1, &timeLow1, &timeLow2, &timeHigh1, &timeHigh2);
-				infeasibleInterval.first = timeLow1 + timeLow2;
-				infeasibleInterval.second = timeHigh1 + timeHigh2;
-				if(infeasibleInterval.second == std::numeric_limits<double>::infinity()) {
-					std::cout << "infinity 1" << std::endl;
-					infeasibleInterval.first = 0.0;
-					infeasibleInterval.second = std::numeric_limits<double>::infinity();
-					return infeasibleInterval;
-					assert(false);
-				}
-
-				if(std::abs(startVelocity + timeHigh1 * -acceleration1) >= maxVelocity) {
-					infeasibleInterval.second = getPLPTime(startVelocity, goalVelocity, distance, maxVelocity, -acceleration1);
-					if(infeasibleInterval.second == std::numeric_limits<double>::infinity()) {
-						std::cout << "infinity 2" << std::endl;
-						assert(false);
-					}
-				}
-			}
+			const double absVelocity = sqrt(maxAcceleration * abs(additionalDistance) + velocity1Squared);
+			const double velocity = sign(additionalDistance) * absVelocity;
+			infeasibleInterval.first = std::abs(velocity1 + velocity2 - 2.0 * velocity) / maxAcceleration;
+			infeasibleInterval.second = std::abs(velocity1 + velocity2 + 2.0 * velocity) / maxAcceleration;
 		}
 
-		return infeasibleInterval;
+		return time;
 	}
 
 	static double getMinTime(const StateVector &state1, const StateVector &state2,
@@ -391,14 +248,16 @@ public:
 	{
 		const Vector distances = state2.template head<dof>() - state1.template head<dof>();
 		double minTime = 0.0;
-		int limitDof = -1; // DOF for which the min time but not the infeasible interval has been calculated yet
 		std::pair<double, double> infeasibleIntervals[dof];
-		Vector firstAccelerations;
 		
 		for(unsigned int i = 0; i < dof && minTime < maxTime; ++i) {
-			const double time = getMinTime1D(state1[dof+i], state2[dof+i], distances[i], maxAccelerations[i], maxVelocities[i], maxTime, firstAccelerations[i]);
-			adjustForInfeasibleIntervals(i, time, maxTime, state1.template tail<dof>(), state2.template tail<dof>(), distances, firstAccelerations, maxAccelerations, maxVelocities,
-			                             minTime, infeasibleIntervals, limitDof);
+			minTime = std::max(minTime, getMinTime1D(state1[dof+i], state2[dof+i], distances[i], maxAccelerations[i], maxVelocities[i], maxTime, infeasibleIntervals[i]));
+			for(unsigned int j = 0; j <= i && minTime < maxTime; ++j) {
+				if(infeasibleIntervals[j].first < minTime && minTime < infeasibleIntervals[j].second) {
+					minTime = infeasibleIntervals[j].second;
+					j = 0;
+				}
+			}
 		}
 
 		assert(minTime < std::numeric_limits<double>::infinity());
